e39273342784df51fe93af9a0cdad23fcac09b0c
Hyune-c <choi8608@gmail.com>
2022-12-20 18:47:19 +0900

    init

---
+++ src/test/kotlin/com/example/kotlinjpatutorial/KotlinJpaTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinjpatutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJpaTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
